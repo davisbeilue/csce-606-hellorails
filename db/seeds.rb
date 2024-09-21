f02da8167d624882b766006e6cca9dec2e577a82
davisbeilue <davis.beilue@tamu.edu>
2024-09-21 18:23:22 -0500

    More seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,15 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Inception', :rating => 'PG-13',
+    :release_date => '16-July-2010'},
+  {:title => 'The Matrix', :rating => 'R',
+    :release_date => '31-Mar-1999'},
+  {:title => 'Interstellar', :rating => 'PG-13',
+    :release_date => '07-Nov-2014'}
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.find_or_create_by(movie)
 end
