8c73f865b6d5efc27fb68c970811e7fde33a71c0
davisbeilue <davis.beilue@tamu.edu>
2024-09-24 20:02:02 -0500

    Comments and ReadMe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,9 @@ module ApplicationHelper
   def sortable(column, title = nil)
     title ||= column.titleize
     css_class = column == sort_column ? "current #{sort_direction}" : nil
+    # if already ascending, we want to switch to descending, and vice-versa
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    # css_class allows us to use current tag so that our css styling applies to the current section only
     link_to title, {sort: column, direction: direction}, {class: css_class}
   end
 end
