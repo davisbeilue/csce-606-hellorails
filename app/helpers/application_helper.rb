3900e2e1175a39a43f9f8d18767d0f7d7b9801bd
davisbeilue <davis.beilue@tamu.edu>
2024-09-24 20:56:10 -0500

    More cleanup - new movie back to link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   # source: http://railscasts.com/episodes/228-sortable-table-columns?view=asciicast
-  def sortable(column, title = nil)
-    css_class = column == sort_column ? "current #{sort_direction}" : nil
+  def sortable(column, movie_prop = nil)
+    selectable_class = column == sort_column ? "selected #{sort_direction}" : nil
     # if already ascending, we want to switch to descending, and vice-versa
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-    # css_class allows us to use current tag so that our css styling applies to the current section only
-    link_to title, {sort: column, direction: direction}, {class: css_class}
+    # selectable_class allows us to use selected tag so that our css styling applies to the selected section only
+    link_to movie_prop, {sort: column, direction: direction}, {class: selectable_class}
   end
 end
